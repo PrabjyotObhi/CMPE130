28411494c6c62a60c5bcd1655d8524eb5cbe0802
PrabjyotObhi <prabjyotobhi@gmail.com>
2019-02-07 14:31:12 -0800

    found out how to run pytest and fixed the functions except for wqu_connected

--- lib/hw1/union_find.py
+++ lib/hw1/union_find.py
@@ -8,7 +8,7 @@
 
 import time
 import random
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 
 class UF(object):
@@ -36,11 +36,11 @@ class UF(object):
         (linear number of array accesses)
 
         """
-        pid = id[p]
-        qid = id[q]
-        for x in len(id):
-            if id[x] == pid:
-                id[x] = qid
+        pid = self.id[p]
+        qid = self.id[q]
+        for x in range(len(self.id)):
+            if self.id[x] is pid:
+                self.id[x] = qid
         return 1
 
     def qf_connected(self, p, q):
@@ -55,8 +55,8 @@ class UF(object):
 
         # qu_find Function
     def qu_find(self, i):
-        while i != id[i]:
-            i = id[i]
+        while i != self.id[i]:
+            i = self.id[i]
 
         return i
 
@@ -68,7 +68,7 @@ class UF(object):
          """
         i = self.qu_find(p)
         j = self.qu_find(q)
-        id[i] = j
+        self.id[i] = j
         return 1
 
     def qu_connected(self, p, q):
@@ -90,7 +90,7 @@ class UF(object):
             return;
 
         if self.sz[i] < self.sz[j]:
-            id[i] = j
+            self.id[i] = j
             self.sz[j] += self.sz[i]
         else:
             id[j] = i
@@ -103,14 +103,16 @@ class UF(object):
          test whether p and q are connected
 
          """
-        return self.id[p] == self.id[q]
+        i = self.pq_find(p)
+        j = self.pq_find(q)
+        return i is j
 
 
     # Need a new path compression find function
     def pq_find(self, i):
-        while i != id[i]:
-            id[i] = id[id[i]]
-            i = id[i]
+        while i != self.id[i]:
+            self.id[i] = self.id[self.id[i]]
+            i = self.id[i]
 
         return i
 
@@ -122,7 +124,7 @@ class UF(object):
          """
         i = self.pq_find(p)
         j = self.pq_find(q)
-        id[i] = j
+        self.id[i] = j
         return 1
 
     def pqu_connected(self, p, q):
@@ -145,13 +147,14 @@ class UF(object):
             return;
 
         if self.sz[i] < self.sz[j]:
-            id[i] = j
+            self.id[i] = j
             self.sz[j] += self.sz[i]
         else:
-            id[j] = i
+            self.id[j] = i
             self.sz[i] += self.sz[j]
 
         return 1
+
 
     def wpqu_connected(self, p, q):
         """Find operation for Weighted path compressed Quick-Union Algorithm.
@@ -191,9 +194,9 @@ if __name__ == "__main__":
 
     # this plots things in log scale (pls google it), you need to add matplotlib to your virtualenv first!
 
-    # plt.plot(set_szs, timing)
-    # plt.xscale('log')
-    # plt.yscale('log')
-    # plt.title('log')
-    # plt.ylabel('some numbers')
-    # plt.show()
+    plt.plot(set_szs, timing)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('log')
+    plt.ylabel('some numbers')
+    plt.show()
