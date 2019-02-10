c48a8a9a733bf56aa30eab9e896ec6d8c7f24a65
PrabjyotObhi <prabjyotobhi@gmail.com>
2019-02-10 01:26:44 -0800

    Started exploring python because it seems very straight forward when compared to C++, dealt with type casting

---
+++ PlayingWithPython.py
@@ -0,0 +1,22 @@
+x = int(8)
+y = int(12.908)
+z = int("42")
+print("x is: ", x, "\n" "y is: ", y, "\n" "z is: ", z)
+print("\n")
+
+s1 = "ayo"
+s2 = "this"
+s3 = "lit"
+s4 = s1 + s2 + s3
+print(s4)
+print("\n")
+
+f1 = float(1)
+f2 = float(2.8)
+f3 = float("3")
+f4 = float("4.2")
+
+print("f1 is: ", f1, "\n" "f2 is: ", f2, "\n" "f3 is: ", f3, "\n" "f4 is: ", f4)
+
+print(int(f4))
+
