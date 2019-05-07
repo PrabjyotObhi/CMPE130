64aad0f9936e8688d5bd7fe149d102e144986473
PrabjyotObhi <prabjyotobhi@gmail.com>
2019-05-07 15:21:08 -0700

    BellmanFord

--- lib/hw5/hw5.py
+++ lib/hw5/hw5.py
@@ -69,11 +69,39 @@ def dijkstra(self, src):
                 dist[v] = dist[u] + self.Graph[u][v]
 
 
-self.printSolution(dist)
+    self.printSolution(dist)
 
-def BellmanFord(graph, source):
 
-    return 1
+def BellmanFord(self, src):
+    # Step 1: Initialize distances from src to all other vertices
+    # as INFINITE
+    dist = [float("Inf")] * self.V
+    dist[src] = 0
+
+    # Step 2: Relax all edges |V| - 1 times. A simple shortest
+    # path from src to any other vertex can have at-most |V| - 1
+    # edges
+    for i in range(self.V - 1):
+        # Update dist value and parent index of the adjacent vertices of
+        # the picked vertex. Consider only those vertices which are still in
+        # queue
+        for u, v, w in self.graph:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                dist[v] = dist[u] + w
+
+                # Step 3: check for negative-weight cycles.  The above step
+    # guarantees shortest distances if graph doesn't contain
+    # negative weight cycle.  If we get a shorter path, then there
+    # is a cycle.
+
+    for u, v, w in self.graph:
+        if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+            print
+            "Graph contains negative weight cycle"
+            return
+
+    # print all distance
+    self.printArr(dist)
 
 def Ford_fullerskon(graph, source, sink):    # you can implement Bfs or dfs to get the path from source(start node) to sink(end node)
 
