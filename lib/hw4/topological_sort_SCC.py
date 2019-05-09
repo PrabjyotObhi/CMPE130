996a4b628d0af4978b5440f0347ad7d13d948549
PrabjyotObhi <prabjyotobhi@gmail.com>
2019-05-09 14:20:57 -0500

    SCC for Homework 4

--- lib/hw4/topological_sort_SCC.py
+++ lib/hw4/topological_sort_SCC.py
@@ -8,8 +8,7 @@ class Graph:
   
     # function to add an edge to graph 
     def addEdge(self,u,v): 
-        self.graph[u].append(v) 
-
+        self.graph[u].append(v)
 
     def sort(self, v, marked, stack):
         marked[v] = True
@@ -27,5 +26,46 @@ class Graph:
         print(stack)
         return 1
 
-    def SCC(self):    # strongly connected componentss
-        return 1
+    def DFS(self, v, visited):
+        visited[v] = True
+        print(v, )
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.DFS(i, visited)
+
+    def fillOrder(self, v, visited, stack):
+        # Mark the current node as visited
+        visited[v] = True
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.fillOrder(i, visited, stack)
+        stack = stack.append(v)
+
+    def Transpose(self):
+        g = Graph(self.V)
+
+        for i in self.graph:
+            for j in self.graph[i]:
+                g.addEdge(j, i)
+        return g
+
+    def SCC(self):
+
+        stack = []
+        visited = [False] * (self.V)
+        for i in range(self.V):
+            if visited[i] == False:
+                self.fillOrder(i, visited, stack)
+
+        gr = self.Transpose()
+
+        # Mark all the vertices as not visited (For second DFS)
+        visited = [False] * (self.V)
+
+        while stack:
+            i = stack.pop()
+            if visited[i] == False:
+                gr.DFS(i, visited)
+                print(" ")
